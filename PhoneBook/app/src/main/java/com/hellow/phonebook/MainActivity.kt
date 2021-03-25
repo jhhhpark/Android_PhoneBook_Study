a594674776cb17b085504b32336ae23b61980443
James_ParkH <znzldh1234@gmail.com>
2021-03-25 17:57:04 +0900

    android_PhoneBook_study ver 1.3

--- PhoneBook/app/src/main/java/com/hellow/phonebook/MainActivity.kt
+++ PhoneBook/app/src/main/java/com/hellow/phonebook/MainActivity.kt
@@ -1,21 +1,33 @@
 package com.hellow.phonebook
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import android.widget.LinearLayout
 import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // list생성 및 phoneBook생성
-        val phoneBook = createFakePhoneBook(20)
-        createPhoneBookList(phoneBook)
+        // 리사이클러뷰 인스턴스 생성
+        val phoneBook_Recycler_View : RecyclerView = findViewById<RecyclerView>(R.id.containerView)
+
+        // 어댑터 및 레이아웃 매니저설정
+        with(phoneBook_Recycler_View) {
+            this.adapter = PhoneBookRecyclerAdapter(
+                    phoneBookList = createFakePhoneBook(50),
+                    inflater = LayoutInflater.from(this@MainActivity),
+                    activity = this@MainActivity)
+            this.layoutManager = LinearLayoutManager(this@MainActivity)
+        }
     }
 
     // 생성 할 Person의 갯수를 입력하면 해당 갯수만큼 Person 인스턴스를 List에 추가 전화번호부를 return
@@ -64,6 +76,51 @@ class MainActivity : AppCompatActivity() {
     }
 }
 
+// RecyclerView Adapter
+// 리사이클러뷰 어댑터 클래스
+class PhoneBookRecyclerAdapter (
+        val phoneBookList : PhoneBook,
+        val inflater : LayoutInflater,
+        val activity : Activity
+) : RecyclerView.Adapter<PhoneBookRecyclerAdapter.ViewHolder>() {
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val personName : TextView?
+        val personNumber: TextView?
+
+        init {
+            personName = itemView?.findViewById<TextView>(R.id.txHeadName)
+            personNumber = itemView?.findViewById<TextView>(R.id.txPhoneNumber)
+
+            itemView.setOnClickListener {
+
+                // 인텐트 생성
+                val intent = Intent(activity, Detail_PhoneBook::class.java)
+
+                // 인텐트에 데이터 저장
+                intent.putExtra("Name", phoneBookList.personList[adapterPosition].name)
+                intent.putExtra("Number", phoneBookList.personList[adapterPosition].number)
+
+                // 상세 페이지 액티비티 시작
+                activity.startActivity(intent)
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = inflater.inflate(R.layout.numberlist_view, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return phoneBookList.personList.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.personName?.text = phoneBookList.personList[position].name
+        holder.personNumber?.text = phoneBookList.personList[position].number
+    }
+}
+
 // Person인스턴스 List를 가진 PhoneBook 클래스 선언
 class PhoneBook() {
     val personList = ArrayList<Person>()
@@ -72,8 +129,6 @@ class PhoneBook() {
     }
 }
 
-
 // 이름과 전화번호를 가진 Person클래스 선언
 class Person(val name: String, val number: String) {
-
 }
